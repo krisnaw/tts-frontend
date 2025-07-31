2be45b694712c708cfb5ae58799c5b2c1ee03864
krisna <krisna.w2010@gmail.com>
2025-07-31 12:28:56 +0800

    wip ui

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -24,15 +24,11 @@ export async function action({request} : Route.ActionArgs) {
     })
   })
 
-
-  console.log(response)
-
   if (response.status !== 200) {
-    console.log("Something went wrong")
+    return redirect("/login")
   }
 
   const result = await response.json();
-  console.log(result)
 
   session.set('userId', result.user.id);
   session.set('username', result.user.name);
@@ -44,23 +40,11 @@ export async function action({request} : Route.ActionArgs) {
       "Set-Cookie": await commitSession(session),
     },
   });
-
-}
-
-export async function loader({ request }: Route.LoaderArgs) {
-  const endpoint  =  import.meta.env.VITE_API_ENDPOINT;
-  const res = await fetch(`${endpoint}/users`);
-  const result = await res.json();
-  return { endpoint: endpoint, users: result}
 }
 
 export default function Login({loaderData} : Route.ComponentProps) {
-  const {endpoint, users} = loaderData;
-  console.log(users)
-
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      {endpoint}
       <div className="w-full max-w-sm">
         <Form method="POST">
           <LoginForm />
